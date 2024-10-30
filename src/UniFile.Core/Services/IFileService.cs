902e851a42405a7f5ea11f98b747fded7af8bb9d
hasan  khaddour <hasan.b.khaddour@gmail.com>
2024-10-30 19:17:55 +0300

    Update IFileService , Add  Esult as Return Type

--- src/UniFile.Core/Services/IFileService.cs
+++ src/UniFile.Core/Services/IFileService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using FluentResults;
+using System.Threading.Tasks;
 using UniFile.Core.Abstraction;
 
 namespace UniFile.Core.Services
@@ -13,27 +14,27 @@ namespace UniFile.Core.Services
         /// </summary>
         /// <param name="fileUpload">An object containing the file's header and content.</param>
         /// <returns>A task representing the asynchronous operation, with the file's unique key upon creation.</returns>
-        Task<string> CreateFileAsync(FileUpload fileUpload);
+        Task<Result<string>> CreateFileAsync(FileUpload fileUpload);
 
         /// <summary>
         /// Reads the content of the specified file.
         /// </summary>
         /// <param name="fileKey">The unique key of the file to read.</param>
         /// <returns>A task representing the asynchronous operation, with the file's content encapsulated in a <see cref="FileUpload"/> object.</returns>
-        Task<FileUpload> ReadFileAsync(string fileKey);
+        Task<Result<FileUpload>> ReadFileAsync(string fileKey);
 
         /// <summary>
         /// Updates the content of the specified file.
         /// </summary>
         /// <param name="fileUpload">An object containing the updated file's header and content.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        Task UpdateFileAsync(FileUpload fileUpload);
+        Task<Result> UpdateFileAsync(FileUpload fileUpload);
 
         /// <summary>
         /// Deletes the specified file.
         /// </summary>
         /// <param name="fileKey">The unique key of the file to delete.</param>
         /// <returns>A task representing the asynchronous operation.</returns>
-        Task DeleteFileAsync(string fileKey);
+        Task<Result> DeleteFileAsync(string fileKey);
     }
 }
