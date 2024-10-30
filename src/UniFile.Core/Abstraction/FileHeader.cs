a5de10b081c2813d41f3c2317783e3156cf577a5
hasan  khaddour <hasan.b.khaddour@gmail.com>
2024-10-30 19:11:41 +0300

    Add File Abstraction . Add File Header , Add File Upload

---
+++ src/UniFile.Core/Abstraction/FileHeader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniFile.Core.Abstraction
+{
+    public class FileHeader
+    {
+        /// <summary>
+        /// Gets or sets the unique key for the file.
+        /// </summary>
+        public string FileKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content type of the file (e.g., "application/pdf").
+        /// </summary>
+        public string ContentType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the file in bytes.
+        /// </summary>
+        public long Size { get; set; }
+
+        /// <summary>
+        /// Gets or sets additional metadata related to the file.
+        /// </summary>
+        public Dictionary<string, string> Metadata { get; set; } = new Dictionary<string, string>();
+    }
+
+}
---
+++ src/UniFile.Core/Abstraction/FileUpload.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace UniFile.Core.Abstraction
+{
+    public class FileUpload
+    {
+        /// <summary>
+        /// Gets or sets the file header containing metadata.
+        /// </summary>
+        public FileHeader Header { get; set; }
+
+        /// <summary>
+        /// Gets or sets the uploaded file.
+        /// </summary>
+        public IFormFile File { get; set; }
+
+        public FileUpload(FileHeader header, IFormFile file)
+        {
+            Header = header;
+            File = file;
+        }
+    }
+
+}
